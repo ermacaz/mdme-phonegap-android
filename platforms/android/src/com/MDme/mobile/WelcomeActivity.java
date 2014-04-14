7f218c7bc5a7170c4d574adc41e57276e650e91b
MattHamada <mattahamada@gmail.com>
2014-04-14 14:19:18 -0700

    initial commit

---
+++ platforms/android/src/com/MDme/mobile/WelcomeActivity.java
@@ -0,0 +1,23 @@
+package com.MDme.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.apache.cordova.Config;
+import org.apache.cordova.CordovaActivity;
+
+/**
+ * Created by Matt Hamada on 4/14/14.
+ */
+public class WelcomeActivity extends CordovaActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        super.init();
+        // Set by <content src="index.html" /> in config.xml
+        super.loadUrl(Config.getStartUrl());
+        //super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
